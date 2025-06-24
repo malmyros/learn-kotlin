07b73c1432086e2d645a10bd73024a97c33ac01d
michail.almyros <m.almyros@gmail.com>
2025-06-24 10:55:20 +0100

    - Example of Sealed classes

---
+++ src/SealedClasses.kt
@@ -0,0 +1,34 @@
+fun main() {
+
+}
+
+sealed class BVehicle
+
+data class BCar(
+    val manufacturer: String,
+    val model: String
+) : BVehicle()
+
+data class BBicycle(
+    val manufacturer: String,
+) : BVehicle()
+
+data class Tv(
+    val manufacturer: String,
+) : BVehicle()
+
+fun getVehicle(vehicle: BVehicle): String = when (vehicle) {
+    is BCar -> vehicle.manufacturer
+    is BBicycle -> vehicle.manufacturer
+    is Tv -> vehicle.manufacturer
+}
+
+
+sealed class SAnimal {
+
+    val legs: Int = 4
+
+    abstract val name: String
+
+    abstract fun eat(): String
+}
